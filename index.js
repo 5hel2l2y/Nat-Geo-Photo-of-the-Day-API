839ca2acaa6fd7b34aa3ad69b70dd67f21cc7e9a
5hel2l2y <sherry.yang.work@gmail.com>
2019-03-20 19:26:45 -0600

    debug - approach without modules

--- index.js
+++ index.js
@@ -5,62 +5,25 @@ const path = require('path');
 const natgeo = require('national-geographic-api').NationalGeographicAPI;
 const app = express();
 
-/**
- * Photo Module
- */
-var photo = function () {
-    var self = this;
 
-    self.port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-    /**
-     *
-     */
-    self.enableCors = function () {
-        app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
-    };
+app.get('/', function (req, res) {
+    res.send(JSON.stringify({ Hello: "World"}));
+});
 
+app.get('/api/dailyphoto', async(req, res, next) => {
+    const result = await natgeo.getPhotoOfDay();
 
-    /**
-     *
-     */
-    self.run = function (options) {
-        if (typeof options.port == "number") {
-            self.port = options.port;
-        }
+    res.jsonp({ 
+        all:            result,
+        src:            result.data[0].attributes.image.uri,
+        alt:            result.data[0].attributes.image.title,
+        description:    result.data[0].attributes.image.alt_text,
+        credit:         result.data[0].attributes.image.credit
+    });
+});
 
-        if (options.cors) {
-            self.enableCors();
-        }
-
-        app.get('/', function (req, res) {
-            res.send(JSON.stringify({ Hello: "World"}));
-        });
-
-        app.get('/api/dailyphoto', async(req, res, next) => {
-            const result = await natgeo.getPhotoOfDay();
-
-            res.jsonp({ 
-                all:            result,
-                src:            result.data[0].attributes.image.uri,
-                alt:            result.data[0].attributes.image.title,
-                description:    result.data[0].attributes.image.alt_text,
-                credit:         result.data[0].attributes.image.credit
-            });
-        });
-
-        app.listen(self.port, function() {
-            console.log("Example app listening on port 3000!");
-        });
-    };
-
-
-    return self;
-}();
-
-
-module.exports = photo;
+app.listen(port, function() {
+    console.log("Listening on port 3000!");
+});
