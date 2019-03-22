1d236af3f5b579f18272cbeffdae149a7b90101c
5hel2l2y <sherry.yang.work@gmail.com>
2019-03-22 06:17:56 -0600

    debug - async function

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const express = require('express');
-const natgeo = require('national-geographic-api').NationalGeographicAPI;
-const app = express();
+var express = require('express');
+var natgeo = require('national-geographic-api').NationalGeographicAPI;
+var app = express();
 
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
 
-const port = process.env.PORT || 8888;
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: "World"}));
 });
 
-app.get('/api/dailyphoto', async(req, res, next) => {
-    const result = await natgeo.getPhotoOfDay();
-
-    res.jsonp({ 
-        all:            result,
-        src:            result.data[0].attributes.image.uri,
-        alt:            result.data[0].attributes.image.title,
-        description:    result.data[0].attributes.image.alt_text,
-        credit:         result.data[0].attributes.image.credit
+app.get('/api/dailyphoto', function(req, res) {
+    natgeo.getPhotoOfDay().then((result) => {
+        res.jsonp({ 
+            all:            result,
+            src:            result.data[0].attributes.image.uri,
+            alt:            result.data[0].attributes.image.title,
+            description:    result.data[0].attributes.image.alt_text,
+            credit:         result.data[0].attributes.image.credit
+        });
+    }, (reason) => {
+        handleError(res, "Failed to get dailyphoto.", 400);
     });
-});
-
-app.listen(port, function() {
-    console.log("Listening on port 8888!");
 });
